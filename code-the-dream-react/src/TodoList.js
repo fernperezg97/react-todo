df2cdc881f09aacf246e87962fc0ea338749f15f
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2023-07-05 15:14:51 -0500

    Add new todo to list -- with original todoList variable DELETED

--- code-the-dream-react/src/TodoList.js
+++ code-the-dream-react/src/TodoList.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import { TodoListItem } from './TodoListItem';
 
-const todoList = [
-    {
-      objectID: '1',
-      title: 'Complete Assignment',
-    },
-    {
-      objectID: '2',
-      title: 'Take Dog on Walk',
-    },
-    {
-      objectID: '3',
-      title: 'Wash Dishes',
-    }
-  ];
 
 function TodoList(props) {
     return (
