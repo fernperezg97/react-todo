08a9ab66e8e3c0a85b587d507baa348d1d24a883
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2023-07-18 18:16:35 -0500

    Added autoFocus to input

--- code-the-dream-react/src/InputWithLabel.js
+++ code-the-dream-react/src/InputWithLabel.js
@@ -4,7 +4,7 @@ function InputWithLabel(props) {
     return (
         <>
             <label>{props.children} </label>
-            <input id="todoTitle" type="text" name="title" value={props.todoTitle} onChange={props.handleTitleChange}></input>
+            <input id="todoTitle" type="text" name="title" value={props.todoTitle} onChange={props.handleTitleChange} autoFocus ></input>
             <input type="submit" value="Add"/>
         </>
     )
