b50da19b6235215d106e0e327948218dd9c0d117
Fernanda Pérez Gutiérrez <fernandaperezgutierrez1@gmail.com>
2023-07-18 21:57:51 -0500

    Add autoFocus to Input -> part 2

--- code-the-dream-react/src/InputWithLabel.js
+++ code-the-dream-react/src/InputWithLabel.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 function InputWithLabel(props) {
+    const inputRef = useRef(null);
+    useEffect(() => {
+        inputRef.current.focus(); // Automatically focus the input when the component mounts
+    });
     return (
         <>
             <label>{props.children} </label>
-            <input id="todoTitle" type="text" name="title" value={props.todoTitle} onChange={props.handleTitleChange} autoFocus ></input>
+            <input id="todoTitle" type="text" name="title" value={props.todoTitle} onChange={props.handleTitleChange} ref={inputRef} ></input>
             <input type="submit" value="Add"/>
         </>
     )
